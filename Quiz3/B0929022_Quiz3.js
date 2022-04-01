46429609b6e65389264daffb8c37b36071ff09ba
YYaHan <100269149+YYaHan@users.noreply.github.com>
2022-04-01 16:46:27 +0800

    add Quiz3

---
+++ Quiz3/B0929022_Quiz3.js
@@ -0,0 +1,10 @@
+const log = document.getElementById('content');
+
+document.addEventListener('keydown', logkey);
+
+function logkey(e) {
+    if(65 <= e.keycode && keycode <= 112)
+    {
+        log.textcontent += $('e.key');
+    }
+}
