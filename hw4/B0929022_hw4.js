f1b642bc367cbfa364d04a99aaa2131969f0e411
YYaHan <100269149+YYaHan@users.noreply.github.com>
2022-04-08 17:40:17 +0800

    Update B0929022_hw4.js

--- hw4/B0929022_hw4.js
+++ hw4/B0929022_hw4.js
@@ -1,22 +1,12 @@
-//上次用DOM,這次用陣列
 var img = new Array();
-img = ['id="A" src=img/A.png>', 'id="B" src=img/B.png>', 'id="C" src=img/C.png>', 'id="D" src=img/D.png>',
-    'id="E" src=img/E.png>', 'id="F" src=img/F.png>', 'id="G" src=img/G.png>', 'id="H" src=img/H.png>', 'id="I" src=img/I.png>',
-    'id="J" src=img/J.png>', 'id="K" src=img/K.png>', 'id="L" src=img/L.png>', 'id="M" src=img/M.png>', 'id="N" src=img/N.png>',
-    'id="O" src=img/O.png>', 'id="P" src=img/P.png>', 'id="Q" src=img/Q.png>', 'id="R" src=img/R.png>', 'id="S" src=img/S.png>',
-    'id="T" src=img/T.png>', 'id="U" src=img/U.png>', 'id="V" src=img/V.png>', 'id="W" src=img/W.png>', 'id="X" src=img/X.png>',
-    'id="Y" src=img/Y.png>', 'id="Z" src=img/Z.png>',];
+img = ['id="A" src=img/A.png>', 'id="B" src=img/B.png>', 'id="C" src=img/C.png>', 'id="D" src=img/D.png>', 'id="E" src=img/E.png>', 'id="F" src=img/F.png>', 'id="G" src=img/G.png>', 'id="H" src=img/H.png>', 'id="I" src=img/I.png>', 'id="J" src=img/J.png>', 'id="K" src=img/K.png>', 'id="L" src=img/L.png>', 'id="M" src=img/M.png>', 'id="N" src=img/N.png>', 'id="O" src=img/O.png>', 'id="P" src=img/P.png>', 'id="Q" src=img/Q.png>', 'id="R" src=img/R.png>', 'id="S" src=img/S.png>', 'id="T" src=img/T.png>', 'id="U" src=img/U.png>', 'id="V" src=img/V.png>', 'id="W" src=img/W.png>', 'id="X" src=img/X.png>', 'id="Y" src=img/Y.png>', 'id="Z" src=img/Z.png>',];
 var time1 = 0;
 var time2 = 0;
-var CODE = ["#A", "#B", "#C", "#D", "#E", "#F", "#G", "#H", "#I",
-    "#J", "#K", "#L", "#M", "#N", "#O", "#P", "#Q", "#R", "#S", "#T",
-    "#U", "#V", "#W", "#X", "#Y", "#Z"];
-var game_id = null;
+CODE = ["#A", "#B", "#C", "#D", "#E", "#F", "#G", "#H", "#I", "#J", "#K", "#L", "#M", "#N", "#O", "#P", "#Q", "#R", "#S", "#T", "#U", "#V", "#W", "#X", "#Y", "#Z"];
 race = ["#race1", "#race2", "#race3", "#race4", "#race5", "#race6"]
 raceClass = ['<img class=ri1', '<img class=ri2', '<img class=ri3', '<img class=ri4', '<img class=ri5', '<img class=ri6'];
 
 $(document).ready(function () {
-
     $(document).keydown(recordTime);
     $(document).keydown(function (key) {
         createCode();
